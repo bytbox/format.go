b1e976842df170a27b61776f446f95759019ad23
Scott Lawrence <bytbox@gmail.com>
2012-07-29 00:47:08 -0700

    rawPart.Write, Write, and Read

--- format/format.go
+++ format/format.go
@@ -1,12 +1,21 @@
 package format
 
 type FormatPart interface {
-	Write(interface{}) string
-	// TODO read
+	Write(interface{}) (string, error)
+	Read(string, interface{}) error
 }
 
 type rawPart struct {
 	data string
+}
+
+func (rp rawPart) Write(interface{}) (string, error) {
+	return rp.data, nil
+}
+
+func (rp rawPart) Read(input string, interface{}) error {
+	// TODO
+	return nil
 }
 
 type fieldPart struct {
@@ -15,15 +24,31 @@ type fieldPart struct {
 
 type Format []FormatPart
 
+func (fmt Format) Write(data interface{}) (string, error) {
+	return "", nil
+}
+
+func (fmt Format) Read(input string, data interface{}) error {
+	return nil
+}
+
 func ParseFormat(fmtStr string) (Format, error) {
 	return nil, nil
 }
 
 func Write(fmtStr string, data interface{}) (string, error) {
-	return "", nil
+	fmt, err := ParseFormat(fmtStr)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Write(data)
 }
 
-func Read(fmtStr string, data interface{}) error {
-	return nil
+func Read(fmtStr string, input string, data interface{}) error {
+	fmt, err := ParseFormat(fmtStr)
+	if err != nil {
+		return err
+	}
+	return fmt.Read(input, data)
 }
 
