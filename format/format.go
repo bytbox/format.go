fdae2ff20ff63a44fb249235e0cb0097eee7b187
Scott Lawrence <bytbox@gmail.com>
2012-07-29 00:48:44 -0700

    Stubbing fieldPart

--- format/format.go
+++ format/format.go
@@ -9,17 +9,27 @@ type rawPart struct {
 	data string
 }
 
-func (rp rawPart) Write(interface{}) (string, error) {
+func (rp rawPart) Write(data interface{}) (string, error) {
 	return rp.data, nil
 }
 
-func (rp rawPart) Read(input string, interface{}) error {
+func (rp rawPart) Read(input string, data interface{}) error {
 	// TODO
 	return nil
 }
 
 type fieldPart struct {
 	name string
+}
+
+func (fp fieldPart) Write(data interface{}) (string, error) {
+	// TODO
+	return "", nil
+}
+
+func (fp fieldPart) Read(input string, data interface{}) error {
+	// TODO
+	return nil
 }
 
 type Format []FormatPart
