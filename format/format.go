db19980111447582704112c291417c428fb0c546
Scott Lawrence <bytbox@gmail.com>
2012-07-29 01:29:04 -0700

    ParseFormat should work

--- format/format.go
+++ format/format.go
@@ -1,5 +1,12 @@
 package format
 
+import (
+	"errors"
+)
+
+// Size of the channel buffer (in runes) used in parsing format strings.
+const parseBufLen = 1 << 8
+
 type FormatPart interface {
 	Write(interface{}) (string, error)
 	Read(string, interface{}) error
@@ -32,20 +39,88 @@ func (fp fieldPart) Read(input string, data interface{}) error {
 	return nil
 }
 
+// A Format describes a particular way of representing a record as a string.
 type Format []FormatPart
 
 func (fmt Format) Write(data interface{}) (string, error) {
-	return "", nil
+	s := ""
+	for _, f := range fmt {
+		ts, err := f.Write(data)
+		s += ts
+		if err != nil {
+			return s, err
+		}
+	}
+	return s, nil
 }
 
 func (fmt Format) Read(input string, data interface{}) error {
 	return nil
 }
 
+// Creates a Format object that can be used to efficiently read and write
+// records from the given format string.
 func ParseFormat(fmtStr string) (Format, error) {
+	var fmt Format
+	var buf string
+	var k bool
+	rchan := make(chan rune, parseBufLen)
+
+	go func() {
+		for _, r := range fmtStr {
+			rchan <- r
+		}
+		close(rchan)
+	}()
+
+	var r rune
+	buf = ""
+
+raw:
+	r, k = <-rchan
+	if !k {
+		if len(buf) > 0 {
+			fmt = append(fmt, rawPart{buf})
+		}
+		goto done
+	}
+	if r == '$' {
+		r, k = <-rchan
+		if !k {
+			return nil, errors.New("Format string ended unexpectedly")
+		}
+		if r == '$' {
+			buf += string('$')
+			goto raw
+		}
+		if r != '{' {
+			return nil, errors.New("Invalid character sequence: $" + string(r))
+		}
+		fmt = append(fmt, rawPart{buf})
+		buf = ""
+		goto field
+	}
+	buf += string(r)
+	goto raw
+
+field:
+	r, k = <-rchan
+	if !k {
+		return nil, errors.New("Format string ended unexpectedly")
+	}
+	if r == '}' {
+		fmt = append(fmt, fieldPart{buf})
+		buf = ""
+		goto raw
+	}
+	buf += string(r)
+	goto field
+
+done:
 	return nil, nil
 }
 
+// See ParseFormat for a description of the format string.
 func Write(fmtStr string, data interface{}) (string, error) {
 	fmt, err := ParseFormat(fmtStr)
 	if err != nil {
@@ -54,6 +129,7 @@ func Write(fmtStr string, data interface{}) (string, error) {
 	return fmt.Write(data)
 }
 
+// See ParseFormat for a description of the format string.
 func Read(fmtStr string, input string, data interface{}) error {
 	fmt, err := ParseFormat(fmtStr)
 	if err != nil {
@@ -61,4 +137,3 @@ func Read(fmtStr string, input string, data interface{}) error {
 	}
 	return fmt.Read(input, data)
 }
-
--- format/format_test.go
+++ format/format_test.go
@@ -4,13 +4,24 @@ import (
 	"testing"
 )
 
+type test_ParseFormat struct {
+	formatString string
+	format       Format
+}
+
+var tests_ParseFormat []test_ParseFormat = []test_ParseFormat{}
+
+func TestParseFormat(t *testing.T) {
+	for _, test := range tests_ParseFormat {
+		println(test.formatString)
+	}
+}
+
 type test_Write struct {
 	formatString string
 }
 
-var tests_Write []test_Write = []test_Write{
-
-}
+var tests_Write []test_Write = []test_Write{}
 
 func TestWrite(t *testing.T) {
 	for _, test := range tests_Write {
@@ -21,4 +32,3 @@ func TestWrite(t *testing.T) {
 func TestRead(t *testing.T) {
 
 }
-
